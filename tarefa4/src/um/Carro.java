cb4db230ba18f96da18d7e0fc55e7b9675f8af1c
Danilo Ferreira <danilosfe@yahoo.com.br>
2022-03-02 13:38:26 -0300

    Tarefa 4 Mentorama

---
+++ tarefa4/src/um/Carro.java
@@ -0,0 +1,168 @@
+package um;
+
+public class Carro {
+
+	//public static final String VERMELHO = "vermelho";
+	//public static final String PRETA = "preta";
+	
+	
+	private int quantidadePneus;
+	private int quantidadeCalotas;
+	private int quantidadeParafusosPneu;
+	private int quantidadePortas;
+	private String cor;
+	private String arCondicionado;
+	private String direcaoHidraulica;
+	private String bancoCouro;
+	private String cambioAutomatico;
+	private String tetoSolar;
+	
+	
+	
+		
+	public int getQuantidadePneus() {
+		return quantidadePneus;
+	}
+
+
+
+
+	public void setQuantidadePneus(int quantidadePneus) {
+		this.quantidadePneus = quantidadePneus;
+	}
+
+
+
+
+	public int getQuantidadeCalotas() {
+		return quantidadeCalotas;
+	}
+
+
+
+
+	public void setQuantidadeCalotas(int quantidadeCalotas) {
+		this.quantidadeCalotas = quantidadeCalotas;
+	}
+
+
+
+
+	public int getQuantidadeParafusosPneu() {
+		return quantidadeParafusosPneu;
+	}
+
+
+
+
+	public void setQuantidadeParafusosPneu(int quantidadeParafusosPneu) {
+		this.quantidadeParafusosPneu = quantidadeParafusosPneu;
+	}
+
+
+
+
+	public int getQuantidadePortas() {
+		return quantidadePortas;
+	}
+
+
+
+
+	public void setQuantidadePortas(int quantidadePortas) {
+		this.quantidadePortas = quantidadePortas;
+	}
+
+
+
+
+	public String getCor() {
+		return cor;
+	}
+
+
+
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+
+
+
+	public String getArCondicionado() {
+		return arCondicionado;
+	}
+
+
+
+
+	public void setArCondicionado(String arCondicionado) {
+		this.arCondicionado = arCondicionado;
+	}
+
+
+
+
+	public String getDirecaoHidraulica() {
+		return direcaoHidraulica;
+	}
+
+
+
+
+	public void setDirecaoHidraulica(String direcaoHidraulica) {
+		this.direcaoHidraulica = direcaoHidraulica;
+	}
+
+
+
+
+	public String getBancoCouro() {
+		return bancoCouro;
+	}
+
+
+
+
+	public void setBancoCouro(String bancoCouro) {
+		this.bancoCouro = bancoCouro;
+	}
+
+
+
+
+	public String getCambioAutomatico() {
+		return cambioAutomatico;
+	}
+
+
+
+
+	public void setCambioAutomatico(String cambioAutomatico) {
+		this.cambioAutomatico = cambioAutomatico;
+	}
+
+
+
+
+	public String getTetoSolar() {
+		return tetoSolar;
+	}
+
+
+
+
+	public void setTetoSolar(String tetoSolar) {
+		this.tetoSolar = tetoSolar;
+	}
+
+
+
+
+	public void imprimeValores() {
+		System.out.println("A quantidade de pneus é: " + getQuantidadePneus());
+		System.out.println("A quantidade de calotas é: " + getQuantidadeCalotas());
+		System.out.println("A quantidade de parafusos é: " + getQuantidadeParafusosPneu());
+	}
+}
---
+++ tarefa4/src/um/Main.java
@@ -0,0 +1,75 @@
+package um;
+
+import um.Carro;
+
+import java.util.Scanner;
+
+
+public class Main { 
+	public static void main(String... args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Informe a quantidade de pneus: ");
+		int quantidadePneus = scanner.nextInt();
+		
+		System.out.print("Informe a quantidade de calotas: ");
+		int quantidadeCalotas = scanner.nextInt();
+	
+		int quantidadeParafusosPneu = quantidadePneus * 4;
+		
+		System.out.print("Informe a quantidade de portas 3 ou 5: ");
+		int quantidadePortas = scanner.nextInt();
+		
+		System.out.print("Informe a cor desejada: ");
+		String cor = scanner.next();
+		
+		System.out.print("Deseja direção hidráulica? Sim ou Não ");
+		String direcaoHidraulica = scanner.next();
+		
+		System.out.print("Deseja banco de couro? Sim ou Não ");
+		String bancoCouro = scanner.next();
+		
+		System.out.print("Deseja câmbio automático? Sim ou Não ");
+		String cambioAutomatico = scanner.next();
+		
+		System.out.print("Deseja teto solar? Sim ou Não ");
+		String tetoSolar = scanner.next();
+		
+		System.out.print("Informe a se deseja ar condicionado? Sim ou Não ");
+		String arCondicionado = scanner.next();
+		
+		
+		Carro novoCarro = new Carro();
+		novoCarro.setQuantidadePneus(quantidadePneus);
+		novoCarro.setQuantidadeCalotas(quantidadeCalotas);
+		novoCarro.setQuantidadeParafusosPneu(quantidadeParafusosPneu);
+		novoCarro.setQuantidadePortas(quantidadePortas);
+		novoCarro.setCor(cor);
+		novoCarro.setDirecaoHidraulica(direcaoHidraulica);
+		novoCarro.setBancoCouro(bancoCouro);
+		novoCarro.setCambioAutomatico(cambioAutomatico);
+		novoCarro.setTetoSolar(tetoSolar);
+		novoCarro.setArCondicionado(arCondicionado);
+		
+		System.out.println("Carro montado com sucesso!");
+		System.out.println("Quantidade de pneus: " + novoCarro.getQuantidadePneus());
+		System.out.println("Quantidade de calotas: " + novoCarro.getQuantidadeCalotas());
+		System.out.println("Quantidade de parafusos: " + novoCarro.getQuantidadeParafusosPneu());
+		System.out.println("Quantidade de portas: " + novoCarro.getQuantidadePortas());
+		System.out.println("A cor do carro é: " + novoCarro.getCor());
+		System.out.println("Direção hidráulica: " + novoCarro.getDirecaoHidraulica());
+		System.out.println("Banco de couro: " + novoCarro.getBancoCouro());
+		System.out.println("Câmbio automático: " + novoCarro.getCambioAutomatico());
+		System.out.println("Teto solar: " + novoCarro.getTetoSolar());
+		System.out.println("Ar-condicionado: " + novoCarro.getArCondicionado());
+		
+		
+		
+		
+		
+		
+			}
+
+}
+ 
